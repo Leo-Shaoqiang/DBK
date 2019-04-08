3f805ee20d1a77976a9c82bfd8a1d33bdd0253da
c03311 <44341821+c03311@users.noreply.github.com>
2019-04-08 18:32:38 +0800

    Update IndexCommend.vue

--- src/components/IndexCommend.vue
+++ src/components/IndexCommend.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
+    <div class="mainpagecommand">
         <el-button type="warning"  class="button-commend" >推荐</el-button>
-        <span class="hot-list" >按热度排名</span>
+        <br>
+    <div>
+          <p>按热度推荐</p><br>
+    </div>
+        <div style="float:left;"><img src="../assets/imgs/icons/u=2165492359,3798978431&fm=26&gp=0.jpg" width="25px"><span id="usersname">用户ID</span></div>
+    <div>
+		    <img src="../assets/imgs/icons/u=919262547,2004138580&fm=26&gp=0.jpg" width="200px;">
+	    	<p style="width:200px;">艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯 Michelle Williams 饰）相恋多年，彼此之间感情十分要好。安妮是一名律师，接手了生命基金会的案件，在女友的邮箱里，艾迪发现了基金会老板德雷克不为人知的秘密....</p>
+    </div>
+        <ul class="communication">
+            <li><img src="../assets/imgs/icons/赞1.png"    width="25px" ></li>
+            <li><img src="../assets/imgs/icons/星星1.png"  width="25px"></li>
+            <li><img src="../assets/imgs/icons/评论1.png" width="25px"></li>
+            <li><img src="../assets/imgs/icons/分享.png"  width="23px"></li>
+        </ul>
     </div>
 </template>
 
@@ -13,22 +27,32 @@
 
 <style>
 /* 首页推荐 */
-
+.mainpagecommand
+{
+  margin-top:20px;
+  margin-left:20px;
+  float: left;
+}
   /* 推荐按钮 */
   .el-button--warning{
     background-color: #FF9D00 !important;
   }
   .button-commend{
-    position: relative;
     float: left;
     left: 100px;
     top: 80px;
   }
-  .hot-list{
-    position: relative;
-    display: inline-block;
+  .communication
+{
+    list-style-type:none;
     float: left;
-    left: 200px;
-    top:90px;
-  }
+    margin-top:10px;
+    margin-left:10px;
+}
+.communication li
+{
+    display: inline;
+    margin-right:10px;
+    cursor: pointer;
+}
 </style>

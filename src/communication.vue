39db5a9c9528bf39d85d253004a59eaa90c88b0b
c03311 <44341821+c03311@users.noreply.github.com>
2019-04-08 17:12:59 +0800

    Add files via upload

---
+++ src/communication.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <img src="icons/u=2165492359,3798978431&fm=26&gp=0.jpg" width="50px"><span id="usersname">用户ID</span>
+	<div>
+		<img src="icons/u=919262547,2004138580&fm=26&gp=0.jpg" width="200px;">
+		<p style="width:200px;">艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯 Michelle Williams 饰）相恋多年，彼此之间感情十分要好。安妮是一名律师，接手了生命基金会的案件，在女友的邮箱里，艾迪发现了基金会老板德雷克不为人知的秘密....</p>
+	</div>
+        <ul class="communication">
+            <li><img src="../assets/imgs/icons/赞1.png"    width="25px" ></li>
+            <li><img src="../assets/imgs/icons/星星1.png"  width="25px"></li>
+            <li><img src="../assets/imgs/icons/评论1.png" width="25px"></li>
+            <li><img src="../assets/imgs/icons/分享.png"  width="23px"></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+.communication
+{
+    list-style-type:none;
+}
+.communication li
+{
+    display: inline;
+    margin-right:5px;
+    cursor: pointer;
+}
+</style>
+

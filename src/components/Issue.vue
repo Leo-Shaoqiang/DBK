7a9b16ee565751ce45fc12f4a579bcdf46ee16b6
ccw <595272421@qq.com>
2019-04-14 16:13:48 +0800

    change

--- src/components/Issue.vue
+++ src/components/Issue.vue
@@ -1,46 +1,80 @@
 <template>
     <div>
+	<link rel="stylesheet" href="../themes/default/default.css" />
+	<Navbar></Navbar>
 	<div class="messagecommand">
 		<div>
-			<dl>相关推荐</dl>
+			<span>相关推荐</span>
 			<div style="border:1px solid black;width:200px;height:300px;"></div>
 		</div>
 		<div>
-			<dl>我的文章</dl>
+			<span>我的文章</span><br>
 			<div style="border:1px solid black;width:100px;height:200px;"></div>
 		</div>
 		<div>
-			<dl>我的草稿</dl>
+			<span>我的草稿</span>
 			<div style="border:1px solid black;width:100px;height:150px;"></div>
 		</div>
 	</div>
 	<div class="compile">
-		标题:<el-input v-model="input" placeholder="请输入内容"></el-input><br>
-		标签:<el-input v-model="input" placeholder="请输入内容"></el-input><br>
-	<el-input
-  		type="textarea"
-  		:rows="2"
-  		placeholder="请输入内容"
-  		v-model="textarea">
-	</el-input>
-		<el-button type="warning" class="button-hot">提交</el-button>
-		<el-button type="warning" class="button-hot">存草稿</el-button>
+		<form>
+		<input type="text" placeholder="请输入标题" class="inputstyle"><br>
+		<input type="text" placeholder="请输入标签" class="inputstyle"><br>
+		<textarea name="content" style="width:800px;height:400px;">KindEditor</textarea><br>
+		<div class="buttons">
+		<button style="float:left;margin-left:20%;">提交</button>
+		<button style="float:right;margin-right:20%;">暂时保存</button>
+		</div>
+		</form>
 	</div>
 </div>
 </template>
 <script>
+import Navbar from '@/views/Navbar.vue';
 export default {
-    
+    components: {
+      Navbar,
+	},
 }
 </script>
 <style>
 	.messagecommand
 	{
 		float:left;
+		margin-top:20px;
 	}
 	.compile
 	{
-		margin-left:25%;
+		float: right;
+		margin-top: 20px;
+		margin-right:10%;
+		width:800px;
+	}
+	.inputstyle
+	{
+		width:800px;
+		margin-bottom:25px;
+		height:30px;
+		border-radius:3px;
+		border: solid 1px gray;
+	}
+	textarea
+	{
+		width:800px;
+		height:300px;
+	}
+	.buttons
+	{
+		margin-top:20px;
+	}
+	button
+	{
+		padding:13px 30px;
+		background-color:orange;
+		color:white;
+		border: none;
+		border-radius:3px;
+		cursor: pointer;
 	}
 
 </style>

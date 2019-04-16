ddbf1b2227c77c299ad6310753c8c9834546d707
ccw <595272421@qq.com>
2019-04-16 16:05:59 +0800

    change

--- src/components/IndexCommend.vue
+++ src/components/IndexCommend.vue
@@ -2,90 +2,56 @@
     <div class="index">
         <el-row>
             <IndexCommend></IndexCommend>
-            <SideBar></SideBar>
         </el-row>
         <el-row>
-        <el-col :span="4">
+        <el-col :span="5">
             <div class="box">
-            <img src="" width="25px" style="float:left;">
-            <span id="usersname" style="float:left;">用户ID</span>
+            <img src="../../src/assets/imgs/icons/head2.jpg" width="50px" style="float:left;">
+            <span id="usersname" style="float:left;margin-top:20px;">用户ID</span>
             <br>
-            <img src="" width="200px;">
-            <p style="width:200px;">艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯Michelle Williams饰）相恋多年，彼此之间感情十分要好。安妮是一名律师，接手了生命基金会的案件，在女友的邮箱里，艾迪发现了基金会老板德雷克不为人知的秘密....
+            <img src="../../src/assets/imgs/icons/head1.jpg" width="100px;" style="margin:0 25%;">
+            <p>艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯Michelle Williams饰）相恋多年，彼此之间感情十分要好。
             </p>
             <ul class="communication">
-                <li><img src="../../src/assets/imgs/icons/赞1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/星星1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/评论1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/分享.png" width="23px"></li>
+                <li class="bonus"><img src="../../src/assets/imgs/icons/bonus1.png" width="25px"><img src="../../src/assets/imgs/icons/bonus2.png" width="25px" style="display:none;"></li>
+                <li class="star"><img src="../../src/assets/imgs/icons/star1.png" width="25px"><img src="../../src/assets/imgs/icons/star2.png" width="25px" style="display:none;"></li>
+                <li class="share"><img src="../../src/assets/imgs/icons/share.png" width="23px"></li>
             </ul>
             </div>
         </el-col>
-        <el-col :span="4">
+        <el-col :span="5">
             <div class="box">
-            <img src="" width="25px" style="float:left;">
-            <span id="usersname" style="float:left;">用户ID</span>
+            <img src="../../src/assets/imgs/icons/head2.jpg" width="50px" style="float:left;">
+            <span id="usersname" style="float:left;margin-top:20px;">用户ID</span>
             <br>
-            <img src="" width="200px;">
-            <p style="width:200px;">艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯Michelle Williams饰）相恋多年，彼此之间感情十分要好。安妮是一名律师，接手了生命基金会的案件，在女友的邮箱里，艾迪发现了基金会老板德雷克不为人知的秘密....
+            <img src="../../src/assets/imgs/icons/head1.jpg" width="100px;" style="margin:0 25%;">
+            <p>艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯Michelle Williams饰）相恋多年，彼此之间感情十分要好。
             </p>
             <ul class="communication">
-                <li><img src="../../src/assets/imgs/icons/赞1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/星星1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/评论1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/分享.png" width="23px"></li>
+                <li class="bonus"><img src="../../src/assets/imgs/icons/bonus1.png" width="25px"><img src="../../src/assets/imgs/icons/bonus2.png" width="25px" style="display:none;"></li>
+                <li class="star"><img src="../../src/assets/imgs/icons/star1.png" width="25px"><img src="../../src/assets/imgs/icons/star2.png" width="25px" style="display:none;"></li>
+                <li class="share"><img src="../../src/assets/imgs/icons/share.png" width="23px"></li>
             </ul>
             </div>
         </el-col>
-        <el-col :span="4">
+        <el-col :span="5">
             <div class="box">
-            <img src="" width="25px" style="float:left;">
-            <span id="usersname" style="float:left;">用户ID</span>
+            <img src="../../src/assets/imgs/icons/head2.jpg" width="50px" style="float:left;">
+            <span id="usersname" style="float:left;margin-top:20px;">用户ID</span>
             <br>
-            <img src="" width="200px;">
-            <p style="width:200px;">艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯Michelle Williams饰）相恋多年，彼此之间感情十分要好。安妮是一名律师，接手了生命基金会的案件，在女友的邮箱里，艾迪发现了基金会老板德雷克不为人知的秘密....
+            <img src="../../src/assets/imgs/icons/head1.jpg" width="100px;" style="margin:0 25%;">
+            <p>艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯Michelle Williams饰）相恋多年，彼此之间感情十分要好。
             </p>
             <ul class="communication">
-                <li><img src="../../src/assets/imgs/icons/赞1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/星星1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/评论1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/分享.png" width="23px"></li>
+                <li class="bonus"><img src="../../src/assets/imgs/icons/bonus1.png" width="25px"><img src="../../src/assets/imgs/icons/bonus2.png" width="25px" style="display:none;"></li>
+                <li class="star"><img src="../../src/assets/imgs/icons/star1.png" width="25px"><img src="../../src/assets/imgs/icons/star2.png" width="25px" style="display:none;"></li>
+                <li class="share"><img src="../../src/assets/imgs/icons/share.png" width="23px"></li>
             </ul>
             </div>
         </el-col>
-        <el-col :span="4">
-            <div class="box">
-            <img src="" width="25px" style="float:left;">
-            <span id="usersname" style="float:left;">用户ID</span>
-            <br>
-            <img src="" width="200px;">
-            <p style="width:200px;">艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯Michelle Williams饰）相恋多年，彼此之间感情十分要好。安妮是一名律师，接手了生命基金会的案件，在女友的邮箱里，艾迪发现了基金会老板德雷克不为人知的秘密....
-            </p>
-            <ul class="communication">
-                <li><img src="../../src/assets/imgs/icons/赞1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/星星1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/评论1.png" width="25px"></li>
-                <li><img src="../../src/assets/imgs/icons/分享.png" width="23px"></li>
-            </ul>
-            </div>
-        </el-col>
-        <el-col :span="4">
-            <div class="box">
-            <img src="" width="25px" style="float:left;">
-            <span id="usersname" style="float:left;">用户ID</span>
-            <br>
-            <img src="" width="200px;">
-            <p style="width:200px;">艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯Michelle Williams饰）相恋多年，彼此之间感情十分要好。安妮是一名律师，接手了生命基金会的案件，在女友的邮箱里，艾迪发现了基金会老板德雷克不为人知的秘密....
-            </p>
-            <ul class="communication">
-                <li><img src="" width="25px"></li>
-                <li><img src="" width="25px"></li>
-                <li><img src="" width="25px"></li>
-                <li><img src="" width="23px"></li>
-            </ul>
-            </div>
-        </el-col>
+
         </el-row> 
+        <SideBar></SideBar>
     </div>
 </template>
 <script>
@@ -94,8 +60,40 @@
     export default {
     components: {
         SideBar,
-        ndexCommend
-    }
+        IndexCommend
+    },
+    mounted() {
+        var check1=0;
+        var check2=0;
+        $(".bonus").on('click',function(){
+            if(check1==0)
+            {
+                $(this).children("img:first").hide();
+                $(this).children("img:last").show();
+                check1=1;
+            }
+            else if(check1==1)
+            {
+                $(this).children("img:first").show();
+                $(this).children("img:last").hide();
+                check1=0;
+            }
+         });
+         $(".star").on('click',function(){
+            if(check2==0)
+            {
+                $(this).children("img:first").hide();
+                $(this).children("img:last").show();
+                check2=1;
+            }
+            else if(check2==1)
+            {
+                $(this).children("img:first").show();
+                $(this).children("img:last").hide();
+                check2=0;
+            }
+         });
+    },
 }
 </script>
 <style>
@@ -112,6 +110,26 @@
         border: 1px solid rgb(248, 250, 159);
         margin-left: 0;
         width: 100%;
-        height: 494px;
-    } 
+        height: 400px;
+    }
+    .communication
+    {
+        list-style-type:none;
+        float: left;
+        position: absolute;
+        bottom:10px;
+    }
+    .communication li
+    {
+        display:inline;
+        margin-right:10px;
+        cursor: pointer;
+        margin-left:25px;
+    }
+    p
+    {
+        font-size:15px;
+        margin:7% 5%;
+        width:80%;
+    }
 </style>

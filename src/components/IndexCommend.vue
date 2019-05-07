70d0198496a88dc560a27cf910bd7df0d2c6db36
thelittleshyboy <1073260894@qq.com>
2019-05-07 22:39:24 +0800

    5.7更新

--- src/components/IndexCommend.vue
+++ src/components/IndexCommend.vue
@@ -2,45 +2,94 @@
     <div>
         <div class="index">
             <div class="back">
-            <el-row>
                 <el-row>
-                    <el-col :span="20" style="margin-left:5%;">
-                        <img src="../../src/assets/imgs/icons/head2.jpg" width="50px" style="float:left;">
-                        <span id="usersname" style="float:left;margin-top:20px;">用户ID</span>
-                    </el-col>
-                </el-row>
-                <el-row style="margin-top:2%;margin-bottom:2%;">
-                   
-                    <el-col :span="4" style="margin-left:2%;">
-                            <div class="box-img">
-                        <img src="../../src/assets/imgs/icons/head1.jpg" style="width:200px;height:300px;float:left">
-                    </div>
-                    </el-col>
-                    
-                    <el-col :span="18">
-                        <div class="title">
-                            <h1>关于毒液的简介</h1>
-                        </div>
-                        <div class="box-content">
+                    <el-row>
+                        <el-col :span="20" style="margin-left:5%;">
+                            <img src="../../src/assets/imgs/icons/head2.jpg" width="50px" style="float:left;">
+                            <span id="usersname" style="float:left;margin-top:20px;">用户ID</span>
+                        </el-col>
+                    </el-row>
+                    <el-row style="margin-top:2%;margin-bottom:2%;">
 
-                        <h5>艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯Michelle Williams饰）相恋多年，
-                            彼此之间感情十分要好。
-                            《毒液：致命守护者》是美国哥伦比亚电影公司、腾讯影业、漫威影业联合出品，索尼电影娱乐公司发行的科幻电影，由鲁本·弗雷斯彻
-                            执导，汤姆·哈迪、米歇尔·威廉姆斯、里兹·阿迈德等人主演。影片改编自漫威漫画，讲述了埃迪·布洛克受到不明外星物质共生体的入侵
-                            与控制，成为亦正亦邪的另类超级英雄的故事。该片于2018年10月5日在美国上映，2018年11月9日在中国大陆上映。 >>></h5>
+                        <el-col :span="6" style="margin-left:2%;">
+                            <div class="box-img">
+                                <img src="../../src/assets/imgs/icons/head1.jpg"
+                                    style="width:250px;height:300px;float:left">
+                            </div>
+                        </el-col>
+
+                        <el-col :span="16">
+                            <div class="title">
+                                <h1>关于毒液的简介</h1>
+                            </div>
+                            <div class="box-content">
+
+                                <h5>艾迪（汤姆·哈迪 Tom Hardy 饰）是一位深受观众喜爱的新闻记者，和女友安妮（米歇尔·威廉姆斯Michelle Williams饰）相恋多年，
+                                    彼此之间感情十分要好。
+                                    《毒液：致命守护者》是美国哥伦比亚电影公司、腾讯影业、漫威影业联合出品，索尼电影娱乐公司发行的科幻电影，由鲁本·弗雷斯彻
+                                    执导，汤姆·哈迪、米歇尔·威廉姆斯、里兹·阿迈德等人主演。影片改编自漫威漫画，讲述了埃迪·布洛克受到不明外星物质共生体的入侵
+                                    与控制，成为亦正亦邪的另类超级英雄的故事。该片于2018年10月5日在美国上映，2018年11月9日在中国大陆上映。 >>></h5>
                             </div>
                             <ul class="communication">
+                                <li class="bonus"><img src="../../src/assets/imgs/icons/bonus1.png" width="40px"><img
+                                        src="../../src/assets/imgs/icons/bonus2.png" width="40px" style="display:none;">
+                                </li>
+                                <li class="star"><img src="../../src/assets/imgs/icons/star1.png" width="40px"><img
+                                        src="../../src/assets/imgs/icons/star2.png" width="40px" style="display:none;">
+                                </li>
+                                <li class="share"><img src="../../src/assets/imgs/icons/share.png" width="38px"></li>
+                            </ul>
+                        </el-col>
+                    </el-row>
+
+                </el-row>
+            </div>
+
+            <div class="back">
+                    <el-row>
+                        <el-row>
+                            <el-col :span="20" style="margin-left:5%;">
+                                <img src="../../src/assets/imgs/icons/head2.jpg" width="50px" style="float:left;">
+                                <span id="usersname" style="float:left;margin-top:20px;">用户ID</span>
+                            </el-col>
+                        </el-row>
+                        <el-row style="margin-top:2%;margin-bottom:2%;">
+    
+                            <el-col :span="6" style="margin-left:2%;">
+                                <div class="box-img">
+                                    <img src="../../src/assets/imgs/icons/head1.jpg"
+                                        style="width:250px;height:300px;float:left">
+                                </div>
+                            </el-col>
+    
+                            <el-col :span="16">
+                                <div class="title">
+                                    <h1>关于毒液透露出漫威的反派正义观</h1>
+                                </div>
+                                <div class="box-content">
+    
+                                    <h5>在这部充斥着埃迪的电影中，一个重大的问题是，埃迪作为一个兴奋剂，一个能够超越技术巨人的人，这是难以令人信服的。（《好莱坞报道者》评） [23] 
+                                            《毒液》不算特别糟糕，但也的确不太好。整部影片都极其平庸，结果无法给观众留下记忆点，只能让坐在座位的人感觉麻木，不确定它到底是想拍一部漫改电影还是推翻漫改电影 [13]  。（《娱乐周刊》评）
+                                            这部电影充满精神分裂症的特质，导演旨在打造一部黑暗而又喜怒无常的电影，但汤姆·哈迪把握住了毒液这个角色，只是电影的其他方面都跟不上他的节奏 [13]  。（《美联社》评）
+                                            
+                  
+                                            
+                                            >>></h5>
+                                </div>
+                                <ul class="communication">
                                     <li class="bonus"><img src="../../src/assets/imgs/icons/bonus1.png" width="40px"><img
-                                            src="../../src/assets/imgs/icons/bonus2.png" width="40px" style="display:none;"></li>
+                                            src="../../src/assets/imgs/icons/bonus2.png" width="40px" style="display:none;">
+                                    </li>
                                     <li class="star"><img src="../../src/assets/imgs/icons/star1.png" width="40px"><img
-                                            src="../../src/assets/imgs/icons/star2.png" width="40px" style="display:none;"></li>
+                                            src="../../src/assets/imgs/icons/star2.png" width="40px" style="display:none;">
+                                    </li>
                                     <li class="share"><img src="../../src/assets/imgs/icons/share.png" width="38px"></li>
                                 </ul>
-                        </el-col>
-                </el-row>
-               
-            </el-row>
-            </div>
+                            </el-col>
+                        </el-row>
+    
+                    </el-row>
+                </div>
         </div>
         <div class="sidebar">
             <SideBar></SideBar>
@@ -50,7 +99,7 @@
 <script>
     import SideBar from "@/components/SideBar.vue";
     export default {
-        
+
         components: {
             SideBar,
         },
@@ -85,35 +134,41 @@
     }
 </script>
 <style>
-    .title{
-        border:1px;
-        float:left;
-        margin-left:5%;
+    .title {
+        border: 1px;
+        float: left;
+        margin-left: 5%;
     }
-    .back{
-        border:1px;
-    } 
+
+    .back {
+        border: 1px;
+    }
+
     .back :hover {
-        
-        background-color:rgb(250, 245, 245);
+
+        background-color: rgb(250, 245, 245);
     }
-    .box-content{
-        border:1px;
-        margin-top:5%;
-        margin-left:5%;
+
+    .box-content {
+        border: 1px;
+        margin-top: 10%;
+        margin-left: 5%;
     }
+
     .sidebar {
         position: absolute;
-        top: 0px;
+        top: 100px;
         left: 70%;
         border: 1px;
         margin-left: 5%;
     }
-    .box-img{
-        border:1px;
-        width:200px;
-        height:300px;
+
+    .box-img {
+        border: 1px;
+        width: 200px;
+        height: 300px;
     }
+
     .index {
         margin-top: 3%;
         border: 3px solid yellow;

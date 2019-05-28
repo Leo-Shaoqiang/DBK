27cfa034768117cc6bbdfe1987aa4060e1d18045
fengmengyu <1034426130@qq.com>
2019-05-28 16:03:31 +0800

    change detail.vue

---
+++ src/components/Detail.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+      <h1>热搜榜</h1>
+          <div class="detail-content">
+              <img src="">
+            <p>sssssssssssssssssssssssssssssss</p>
+              
+          </div>
+  </div>   
+</template>
